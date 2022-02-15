a583b489a6914ade6cbe3ff43949d3381be9dc52
Matthieu Parent <matthieuparent@outlook.com>
2022-02-14 22:02:45 -0500

    fichier départ

---
+++ README.md
@@ -0,0 +1,6 @@
+# Danse contre la violence
+Matériel de départ
+
+<br><br><br><hr>
+Préparé par : Matthieu Parent et Jean-François Leblanc  
+_timtools v1.22.0_
---
+++ src/scripts/Main.js
@@ -0,0 +1,16 @@
+/** Componsante Main de TimTools */
+export default class Main {
+  /**
+   * Méthode constructeur
+   * @param {HTMLElement} element - Élément HTML sur lequel la composante est instanciée
+   */
+  constructor(element) {
+    this.element = element;
+    this.init();
+  }
+
+  /**
+   * Méthode d'initialisation
+   */
+  init() {}
+}
